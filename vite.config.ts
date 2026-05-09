67d0332a2f3986b48dc2b4ea9b6c3e556081d07f
Patrick Vargas <patrick.barcellos@hotmail.com.br>
2026-05-09 12:58:36 -0300

    fix(repo): add nitro plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -17,6 +18,7 @@ const config = defineConfig({
 		viteTsConfigPaths({ projects: ["./tsconfig.json"] }),
 		tailwindcss(),
 		tanstackStart(),
+		nitro(),
 		viteReact(),
 	],
 });
